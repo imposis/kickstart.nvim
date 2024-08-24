8c0c28ae8faa2adb7c4682ddbdf8086328ec3b6e
Josef Dvorak <pepadvorak99@seznam.cz>
2024-08-24 15:18:25 +0200

    custom config
    rebase fixes
    
    removed notify
    
    mainly removed because of macro recording
    
    themes and default lsp
    
    Added light/dark theme switch and function signature help
    
    jukit and themes
    
    borders for floating windows and removed f/t jump highlighting
    
    custom themes and keymaps for buffer close and next/previous
    
    colorschemes, some other fixes
    
    removed some comments
    
    Added plugin for highlighting active parameter when writing method signatures
    
    buffer symbols and codeium output in cmp window + jason syntax highlighting
    
    some changes

---
+++ lua/custom/plugins/codeium.lua
@@ -0,0 +1,19 @@
+return {
+  'Exafunction/codeium.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'hrsh7th/nvim-cmp',
+  },
+  config = function()
+    require('codeium').setup {
+      enable_cmp_source = false,
+      virtual_text = {
+        enabled = true,
+        key_bindings = {
+          accept = '<M-a>',
+          clear = '<M-x>',
+        },
+      },
+    }
+  end,
+}
---
+++ lua/custom/plugins/lazygit.lua
@@ -0,0 +1,11 @@
+return {
+  'kdheepak/lazygit.nvim',
+  -- optional for floating window border decoration
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+  },
+  lazy = false,
+  keys = {
+    { '<leader>gg', ':LazyGit<CR>', { desc = 'LazyGit open' } },
+  },
+}
